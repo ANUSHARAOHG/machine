2c71e4788ac7ab827687791c5a9b742fff73e9c2
ANUSHARAOHG <anuraohebbige57@gmail.com>
2023-05-29 10:11:24 +0530

    added

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    // AJAX request to fetch the machine data
+    $.ajax({
+      url: 'machines.json', // Replace 'machines.json' with the path to your JSON file
+      dataType: 'json',
+      success: function(data) {
+        // Iterate over each machine object and create a card
+        $.each(data, function(index, machine) {
+          var cardHtml = `
+            <div class="card">
+              <h3>Machine: ${machine.MACHINE}</h3>
+              <p>Performance: ${machine.PERFORMANCE}</p>
+              <p>Status: ${machine.STATUS}</p>
+              <p>Running Since: ${machine['RUNNING SINCE']}</p>
+              <p>Stopped Since: ${machine['STOPPED SINCE']}</p>
+              <p>Parts Produced: ${machine['PARTS PRODUCED']}</p>
+            </div>
+          `;
+          $('#machine-container').append(cardHtml);
+        });
+      },
+      error: function() {
+        alert('Failed to fetch machine data.');
+      }
+    });
+  });
+  
